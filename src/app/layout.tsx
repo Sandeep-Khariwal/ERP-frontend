71291485b0f67fc5b5e7288b66649808c70f1d1c
sandeep khariwal <sandeepkharival123@gmail.com>
2024-11-24 13:29:00 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import '@mantine/core/styles.css';
+import { MantineProvider } from "@mantine/core";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+       <MantineProvider >
         {children}
+      </MantineProvider>
       </body>
     </html>
   );
