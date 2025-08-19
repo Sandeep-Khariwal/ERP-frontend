807bb399159a9d14d392659effb63b2dad4045d4
sandeep khariwal <sandeepkharival123@gmail.com>
2025-08-19 16:48:23 +0530

    aws url added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-   URL: "https://erp-backend-p5nc.onrender.com",
+ //  URL: "https://erp-backend-p5nc.onrender.com",
+   URL : "http://3.88.157.239:8080",
   //  URL: "http://localhost:5070"
   },
     eslint: {
