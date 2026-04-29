ea3a74adb3ac82e7975e459cc1d606422044ce3d
sandeep khariwal <sandeepkharival123@gmail.com>
2026-04-29 12:26:02 +0530

    mobile resp & dynm img

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-   URL: "http://localhost:8080"
-  //  URL: "https://server.shikshapay.cloud"
+  //  URL: "http://localhost:8080"
+   URL: "https://server.shikshapay.cloud"
   },
   
 };
